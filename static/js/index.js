fc024e61923953ac521c52cb3ebf47306aa6362a
Sergey Antonov <flyker777@yandex.ru>
2019-12-08 17:43:23 +0300

    add MVC for API

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,224 @@
-console.log("Hello!", $)
+let ns = {};
+
+ns.model = (function() {
+    'use strict';
+
+    let $event_pump = $('body');
+
+    return {
+        'read': function() {
+            let ajax_options = {
+                type: 'GET',
+                url: 'api/people',
+                accepts: 'application/json',
+                dataType: 'json'
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_read_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        },
+        create: function(fname, lname) {
+            let ajax_options = {
+                type: 'POST',
+                url: 'api/people',
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify({
+                    person: {
+                        'fname': fname,
+                        'lname': lname
+                    }
+                })
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_create_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        },
+        update: function(fname, lname) {
+            let ajax_options = {
+                type: 'PUT',
+                url: 'api/people/' + lname,
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify({
+                    'fname': fname,
+                    'lname': lname
+                })
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_update_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        },
+        'delete': function(lname) {
+            let ajax_options = {
+                type: 'DELETE',
+                url: 'api/people/' + lname,
+                accepts: 'application/json',
+                contentType: 'plain/text'
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_delete_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        }
+    };
+}());
+
+ns.view = (function() {
+    'use strict';
+
+    let $fname = $('#fname'),
+        $lname = $('#lname');
+
+    // return the API
+    return {
+        reset: function() {
+            $lname.val('');
+            $fname.val('').focus();
+        },
+        update_editor: function(fname, lname) {
+            $lname.val(lname);
+            $fname.val(fname).focus();
+        },
+        build_table: function(people) {
+            let rows = ''
+
+            $('.people table > tbody').empty();
+
+            if (people) {
+                for (let i=0, l=people.length; i < l; i++) {
+                    rows += `<tr><td class="fname">${people[i].fname}</td><td class="lname">${people[i].lname}</td><td>${people[i].timestamp}</td></tr>`;
+                }
+                $('table > tbody').append(rows);
+            }
+        },
+        error: function(error_msg) {
+            $('.error')
+                .text(error_msg)
+                .css('visibility', 'visible');
+            setTimeout(function() {
+                $('.error').css('visibility', 'hidden');
+            }, 3000)
+        }
+    };
+}());
+
+ns.controller = (function(m, v) {
+    'use strict';
+
+    let model = m,
+        view = v,
+        $event_pump = $('body'),
+        $fname = $('#fname'),
+        $lname = $('#lname');
+
+    setTimeout(function() {
+        model.read();
+    }, 100)
+
+    function validate(fname, lname) {
+        return fname !== "" && lname !== "";
+    }
+
+    $('#create').click(function(e) {
+        let fname = $fname.val(),
+            lname = $lname.val();
+
+        e.preventDefault();
+
+        if (validate(fname, lname)) {
+            model.create(fname, lname)
+        } else {
+            alert('Problem with first or last name input');
+        }
+    });
+
+    $('#update').click(function(e) {
+        let fname = $fname.val(),
+            lname = $lname.val();
+
+        e.preventDefault();
+
+        if (validate(fname, lname)) {
+            model.update(fname, lname)
+        } else {
+            alert('Problem with first or last name input');
+        }
+        e.preventDefault();
+    });
+
+    $('#delete').click(function(e) {
+        let lname = $lname.val();
+
+        e.preventDefault();
+
+        if (validate('placeholder', lname)) {
+            model.delete(lname)
+        } else {
+            alert('Problem with first or last name input');
+        }
+        e.preventDefault();
+    });
+
+    $('#reset').click(function() {
+        view.reset();
+    })
+
+    $('table > tbody').on('dblclick', 'tr', function(e) {
+        let $target = $(e.target),
+            fname,
+            lname;
+
+        fname = $target
+            .parent()
+            .find('td.fname')
+            .text();
+
+        lname = $target
+            .parent()
+            .find('td.lname')
+            .text();
+
+        view.update_editor(fname, lname);
+    });
+
+    $event_pump.on('model_read_success', function(e, data) {
+        view.build_table(data);
+        view.reset();
+    });
+
+    $event_pump.on('model_create_success', function(e, data) {
+        model.read();
+    });
+
+    $event_pump.on('model_update_success', function(e, data) {
+        model.read();
+    });
+
+    $event_pump.on('model_delete_success', function(e, data) {
+        model.read();
+    });
+
+    $event_pump.on('model_error', function(e, xhr, textStatus, errorThrown) {
+        let error_msg = textStatus + ': ' + errorThrown + ' - ' + xhr.responseJSON.detail;
+        view.error(error_msg);
+        console.log(error_msg);
+    })
+}(ns.model, ns.view));
