23daeea1fbc8036d9c728364a2461419be4e2f5d
Sergey Antonov <flyker777@yandex.ru>
2019-12-09 20:55:56 +0300

    add relationship one-to-many for person

--- models.py
+++ models.py
@@ -10,6 +10,23 @@ class Person(db.Model):
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
+    notes = db.relationship(
+        "Note",
+        backref='person',
+        cascade='all, delete, delete-orphan',
+        single_parent=True,
+        order_by='desc(Note.timestamp)'
+    )
+
+
+class Note(db.Model):
+    __tablename__ = "note"
+    note_id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.person_id'))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
 
 
 class PersonSchema(ma.ModelSchema):
