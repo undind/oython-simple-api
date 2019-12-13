10481408f3a96ad3e997a2316d82d4dbbd8d0596
Sergey Antonov <flyker777@yandex.ru>
2019-12-13 15:59:09 +0300

    add home MVC

--- static/js/home.js
+++ static/js/home.js
@@ -3,245 +3,74 @@ let ns = {};
 // Create the model instance
 ns.model = (function() {
 
-    let $event_pump = $('body');
-
     return {
-        read: function() {
+        read: function () {
             let ajax_options = {
                 type: 'GET',
-                url: 'api/people',
+                url: '/api/notes',
                 accepts: 'application/json',
                 dataType: 'json'
             };
-            $.ajax(ajax_options)
-            .done(function(data) {
-                $event_pump.trigger('model_read_success', [data]);
-            })
-            .fail(function(xhr, textStatus, errorThrown) {
-                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-            })
-        },
-        create: function(person) {
-            let ajax_options = {
-                type: 'POST',
-                url: 'api/people',
-                accepts: 'application/json',
-                contentType: 'application/json',
-                dataType: 'json',
-                data: JSON.stringify(person)
-            };
-            $.ajax(ajax_options)
-            .done(function(data) {
-                $event_pump.trigger('model_create_success', [data]);
-            })
-            .fail(function(xhr, textStatus, errorThrown) {
-                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-            })
-        },
-        update: function(person) {
-            let ajax_options = {
-                type: 'PUT',
-                url: `api/people/${person.person_id}`,
-                accepts: 'application/json',
-                contentType: 'application/json',
-                dataType: 'json',
-                data: JSON.stringify(person)
-            };
-            $.ajax(ajax_options)
-            .done(function(data) {
-                $event_pump.trigger('model_update_success', [data]);
-            })
-            .fail(function(xhr, textStatus, errorThrown) {
-                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-            })
-        },
-        delete: function(person_id) {
-            let ajax_options = {
-                type: 'DELETE',
-                url: `api/people/${person_id}`,
-                accepts: 'application/json',
-                contentType: 'plain/text'
-            };
-            $.ajax(ajax_options)
-            .done(function(data) {
-                $event_pump.trigger('model_delete_success', [data]);
-            })
-            .fail(function(xhr, textStatus, errorThrown) {
-                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-            })
+
+            return $.ajax(ajax_options)
         }
-    };
+    }
+
 }());
 
 // Create the view instance
 ns.view = (function() {
 
-    let $person_id = $('#person_id'),
-        $fname = $('#fname'),
-        $lname = $('#lname');
+    let table = $('.blog table');
 
     return {
-        reset: function() {
-            $person_id.val('');
-            $lname.val('');
-            $fname.val('').focus();
-        },
-        update_editor: function(person) {
-            $person_id.val(person.person_id);
-            $lname.val(person.lname);
-            $fname.val(person.fname).focus();
-        },
-        build_table: function(people) {
-            let rows = ''
+        build_table: function(data) {
+            let source = $('#blog-table-template').html();
+            let template = Handlebars.compile(source);
+            let html = template({ notes: data });
 
-            // clear the table
-            $('.people table > tbody').empty();
-
-            // did we get a people array?
-            if (people) {
-                for (let i=0, l=people.length; i < l; i++) {
-                    rows += `<tr data-person-id="${people[i].person_id}">
-                        <td class="fname">${people[i].fname}</td>
-                        <td class="lname">${people[i].lname}</td>
-                        <td>${people[i].timestamp}</td>
-                    </tr>`;
-                }
-                $('table > tbody').append(rows);
-            }
-        },
-        error: function(error_msg) {
-            $('.error')
-                .text(error_msg)
-                .css('visibility', 'visible');
-            setTimeout(function() {
-                $('.error').css('visibility', 'hidden');
-            }, 3000)
+            table.append(html);
         }
-    };
+    }
+
 }());
 
 // Create the controller
 ns.controller = (function(m, v) {
 
-    let model = m,
-        view = v,
-        $event_pump = $('body'),
-        $person_id = $('#person_id'),
-        $fname = $('#fname'),
-        $lname = $('#lname');
+    let model = m;
+    let view = v;
 
-
-    // Get the data from the model after the controller is done initializing
-    setTimeout(function() {
-        model.read();
+    setTimeout(() => {
+        model.read()
+            .done(data => {
+                view.build_table(data);
+            })
+            .fail((xhr, textStatus, errorThrow) => {
+                error_handler(xhr, textStatus, errorThrow)
+            })
     }, 0)
+    
+    function error_handler(xhr, textStatus, errorThrow) {
+        console.log(xhr)
+        let errorsMessage = `${textStatus}: ${errorThrow} - ${xhr.responseBody}`;
 
-    // Validate input
-    function validate(fname, lname) {
-        return fname !== "" && lname !== "";
+        view.error(errorsMessage);
     }
 
-    // Create our event handlers
-    $('#create').click(function(e) {
-        let fname = $fname.val(),
-            lname = $lname.val();
+    $('table').on('doubleclick', 'tbody td.name', e => {
+        let target = $(e.target).parent();
+        let personId = target.data('person_id')
 
-        e.preventDefault();
-
-        if (validate(fname, lname)) {
-            model.create({
-                'fname': fname,
-                'lname': lname,
-            })
-        } else {
-            alert('Problem with first or last name input');
-        }
-    });
-
-    $('#update').click(function(e) {
-        let person_id = $person_id.val(),
-            fname = $fname.val(),
-            lname = $lname.val();
-
-        e.preventDefault();
-
-        if (validate(fname, lname)) {
-            model.update({
-                person_id: person_id,
-                fname: fname,
-                lname: lname,
-            })
-        } else {
-            alert('Problem with first or last name input');
-        }
-        e.preventDefault();
-    });
-
-    $('#delete').click(function(e) {
-        let person_id = $person_id.val();
-
-        e.preventDefault();
-
-        if (validate('placeholder', lname)) {
-            model.delete(person_id)
-        } else {
-            alert('Problem with first or last name input');
-        }
-        e.preventDefault();
-    });
-
-    $('#reset').click(function() {
-        view.reset();
+        window.location = `/people/${personId}`;
     })
 
-    $('table > tbody').on('dblclick', 'tr', function(e) {
-        let $target = $(e.target),
-            person_id,
-            fname,
-            lname;
+    $('table').on('doubleclick', 'tbody td.content', e => {
+        let target = $(e.target).parent();
+        let personId = target.data('person_id');
+        let noteId = target.data('note_id')
 
-        person_id = $target
-            .parent()
-            .attr('data-person-id');
-
-        fname = $target
-            .parent()
-            .find('td.fname')
-            .text();
-
-        lname = $target
-            .parent()
-            .find('td.lname')
-            .text();
-
-        view.update_editor({
-            person_id: person_id,
-            fname: fname,
-            lname: lname,
-        });
-    });
-
-    // Handle the model events
-    $event_pump.on('model_read_success', function(e, data) {
-        view.build_table(data);
-        view.reset();
-    });
-
-    $event_pump.on('model_create_success', function(e, data) {
-        model.read();
-    });
-
-    $event_pump.on('model_update_success', function(e, data) {
-        model.read();
-    });
-
-    $event_pump.on('model_delete_success', function(e, data) {
-        model.read();
-    });
-
-    $event_pump.on('model_error', function(e, xhr, textStatus, errorThrown) {
-        let error_msg = textStatus + ': ' + errorThrown;
-        view.error(error_msg);
-        console.log(error_msg);
+        window.location = `/people/${personId}/notes/${noteId}`;
     })
+
 }(ns.model, ns.view));
