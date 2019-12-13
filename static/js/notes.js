747e2cacfde371c9071cbf7fcada993447ceb8b6
Sergey Antonov <flyker777@yandex.ru>
2019-12-13 19:30:56 +0300

    add notes MVC

--- static/js/notes.js
+++ static/js/notes.js
@@ -1,19 +1,8 @@
 const ns = {};
 
-ns.model = (function() {
-
+ns.model = (function(){
     return {
-        read: () => {
-            const ajax_options = {
-                type: "GET",
-                url: "/api/people",
-                accept: "application/json",
-                dataType: "json"
-            };
-
-            return $.ajax(ajax_options);
-        },
-        read_one: person_id => {
+        read: person_id => {
             const ajax_options = {
                 type: "GET",
                 url: `/api/people/${person_id}`,
@@ -23,34 +12,44 @@ ns.model = (function() {
 
             return $.ajax(ajax_options);
         },
-        create: person => {
+        read_one: (person_id, note_id) => {
+            const ajax_options = {
+                type: "GET",
+                url: `/api/people/${person_id}/notes/${note_id}`,
+                accept: "application/json",
+                dataType: "json"
+            };
+
+            return $.ajax(ajax_options);
+        },
+        create: (person_id, note) => {
             const ajax_options = {
                 type: "POST",
-                url: "/api/people",
+                url: `/api/people/${person_id}/notes`,
                 accept: "application/json",
                 contentType: 'application/json',
                 dataType: "json",
-                data: JSON.stringify(person)
+                data: JSON.stringify(note)
             };
 
             return $.ajax(ajax_options)
         },
-        update: person => {
+        update: (person_id, note) => {
             const ajax_options = {
                 type: "PUT",
-                url: `/api/people/${person.person_id}`,
+                url: `/api/people/${person_id}/notes/${note.note_id}`,
                 accept: "application/json",
                 contentType: 'application/json',
                 dataType: "json",
-                data: JSON.stringify(person)
+                data: JSON.stringify(note)
             };
 
             return $.ajax(ajax_options)
         },
-        delete: person_id => {
+        delete: (person_id, note_id) => {
             const ajax_options = {
                 type: "DELETE",
-                url: `/api/people/${person_id}`,
+                url: `/api/people/${person_id}/notes/${note_id}`,
                 accept: "application/json",
                 contentType: 'plain/text'
             };
@@ -58,17 +57,18 @@ ns.model = (function() {
             return $.ajax(ajax_options)
         }
     }
-
 }());
 
-ns.view = (function() {
-
+ns.view = (function(){
     const NEW_NOTE = 0;
     const EXISTING_NOTE = 1;
 
     let person_id = $('#person_id');
     let fname = $('#fname');
     let lname = $('#lname');
+    let $timestamp = $('#timestamp');
+    let $note_id = $('#note_id');
+    let $note = $('#note');
     let $create = $('#create');
     let $update = $('#update');
     let $delete = $('#delete');
@@ -76,16 +76,13 @@ ns.view = (function() {
     return {
         NEW_NOTE: NEW_NOTE,
         EXISTING_NOTE: EXISTING_NOTE,
-
         reset: () => {
-          person_id.text('');
-          lname.val('');
-          fname.val('').focus();
+          $note_id.val('');
+          $note.val('').focus();
         },
-        update_editor: (person) => {
-            person_id.text(person.person_id);
-            lname.val(person.lname);
-            fname.val(person.fname).focus();
+        update_editor: note => {
+            $note_id.text(note.note_id);
+            $note.val(note.content).focus();
         },
         set_button_state: state => {
             if (state === NEW_NOTE) {
@@ -98,58 +95,65 @@ ns.view = (function() {
                 $delete.prop('disabled', false);
             }
         },
-        build_table: (people) => {
-            let source = $('#people-table-template').html();
+        build_table: person => {
+            let source = $('#notes-table-template').html();
             let template = Handlebars.compile(source);
             let html;
 
-            $('.people table > tbody').empty();
+            person_id.text(person.person_id);
+            fname.text(person.fname);
+            lname.text(person.lname);
+            $timestamp.text(person.timestamp);
 
-            if (people) {
-                html = template({ people });
+            $('.notes table > tbody').empty();
+
+            if (person.notes) {
+                html = template({notes: person.notes});
                 $('table').append(html);
             }
         },
-        error: (errorMessage) => {
-            $('.error').text(errorMessage).css('visibility', 'visible');
-            setTimeout(() => {
+        error: errorMessage => {
+            $('.error')
+                .text(errorMessage)
+                .css('visibility', 'visible');
+            setTimeout(function () {
                 $('.error').fadeOut();
             }, 3000)
         }
     }
-
 }());
 
-
-ns.controller = (function(m, v) {
+ns.controller = (function(m, v){
     let model = m;
     let view = v;
     let url_person_id = $('#url_person_id');
-    let $person_id = $('#person_id');
-    let $fname = $('#fname');
-    let $lname = $('#lname');
+    let url_note_id = $('#url_note_id');
+    let $note_id = $('#note_id');
+    let $note = $('#note');
 
     setTimeout(() => {
         view.reset();
-        model.read()
+        model.read(parseInt(url_person_id.val()))
             .done(data => {
                 view.build_table(data);
+                view.update_editor(data);
+                view.set_button_state(view.NEW_NOTE);
             })
-            .fail((xhr, textStatus, errorThrow) => {
-                error_handler(xhr, textStatus, errorThrow)
+            .fail((xhr, textStatus, errorThrown) => {
+                error_handler(xhr, textStatus, errorThrown);
             });
 
-        if (url_person_id.val() !== '') {
-            model.read_one(parseInt(url_person_id.val()))
-                .done((data) => {
+        if (url_note_id.val() !== "") {
+            model.read_one(parseInt(url_person_id.val()), parseInt(url_note_id.val()))
+                .done(data => {
                     view.update_editor(data);
                     view.set_button_state(view.EXISTING_NOTE);
                 })
-                .fail((xhr, textStatus, errorThrow) => {
-                    error_handler(xhr, textStatus, errorThrow)
-                })
+                .fail((xhr, textStatus, errorThrown) => {
+                    error_handler(xhr, textStatus, errorThrown);
+                });
         }
-    }, 0);
+    }, 0)
 
     function error_handler(xhr, textStatus, errorThrow) {
         let errorsMessage = `${textStatus}: ${errorThrow} - ${xhr.responseJSON.detail}`;
@@ -159,55 +163,21 @@ ns.controller = (function(m, v) {
 
     view.set_button_state(view.NEW_NOTE);
 
-    function validate(fname, lname) {
-        return fname !== "" && lname !== "";
+    function validate(note) {
+        return note !== "";
     }
 
     $('#create').click(e => {
-        let fname = $fname.val();
-        let lname = $lname.val();
+        let note = $note.val();
 
         e.preventDefault();
 
-        if (validate(fname, lname)) {
-             model.create({
-                'fname': fname,
-                'lname': lname
+        if (validate(note)) {
+            model.create(parseInt($('#url_person_id').val()), {
+                content: note
             })
                 .done(data => {
-                    model.read()
-                        .done(data => {
-                            view.build_table(data)
-                        })
-                        .fail((xhr, textStatus, errorThrown) => {
-                            error_handler(xhr, textStatus, errorThrown)
-                        });
-                    view.set_button_state(view.NEW_NOTE);
-                })
-                .fail((xhr, textStatus, errorThrown) => {
-                    error_handler(xhr, textStatus, errorThrown)
-                });
-            view.reset();
-        } else {
-            alert('Problem with first or last name input');
-        }
-    });
-
-    $('#update').click(e => {
-        let person_id = parseInt($person_id.text());
-        let fname = $fname.val();
-        let lname = $lname.val();
-
-        e.preventDefault();
-
-        if (validate(fname, lname)) {
-            model.update({
-                person_id: person_id,
-                fname: fname,
-                lname: lname,
-            })
-                .done(data => {
-                    model.read()
+                    model.read(parseInt($('#url_person_id').val()))
                         .done(data => {
                             view.build_table(data);
                         })
@@ -219,22 +189,27 @@ ns.controller = (function(m, v) {
                 })
                 .fail((xhr, textStatus, errorThrown) => {
                     error_handler(xhr, textStatus, errorThrown);
-                })
+                });
 
         } else {
-            alert('Problem with first or last name input');
+            alert('Problem with note input');
         }
     });
 
-    $('#delete').click(e => {
-        let person_id = parseInt($person_id.text());
+    $('#update').click(e => {
+        let person_id = parseInt(url_person_id.val());
+        let note_id = parseInt($note_id.text());
+        let note = $note.val();
 
         e.preventDefault();
 
-        if (validate('placeholder', lname)) {
-            model.delete(person_id)
+        if (validate(note)) {
+            model.update(person_id, {
+                note_id: note_id,
+                content: note
+            })
                 .done(data => {
-                    model.read()
+                    model.read(data.person.person_id)
                         .done(data => {
                             view.build_table(data);
                         })
@@ -245,6 +220,34 @@ ns.controller = (function(m, v) {
                     view.set_button_state(view.NEW_NOTE);
                 })
                 .fail((xhr, textStatus, errorThrown) => {
+                    error_handler(xhr, textStatus, errorThrown);
+                });
+
+        } else {
+            alert('Problem with first or last name input');
+        }
+    });
+
+    $('#delete').click(e => {
+        let person_id = parseInt(url_person_id.val());
+        let note_id = parseInt($note_id.text());
+
+        e.preventDefault();
+
+        if (validate('placeholder', lname)) {
+            model.delete(person_id, note_id)
+                .done(data => {
+                    model.read(parseInt($('#url_person_id').val()))
+                        .done(data => {
+                            view.build_table(data);
+                        })
+                        .fail((xhr, textStatus, errorThrown) => {
+                            error_handler(xhr, textStatus, errorThrown);
+                        });
+                    view.reset();
+                    view.set_button_state(view.NEW_NOTE);
+                })
+                .fail( (xhr, textStatus, errorThrown) => {
                     error_handler(xhr, textStatus, errorThrown);
                 });
 
@@ -258,27 +261,15 @@ ns.controller = (function(m, v) {
         view.set_button_state(view.NEW_NOTE);
     })
 
-    $('table').on('click', 'tbody tr', e => {
+    $('table').on('click', 'tbody tr', (e) => {
         let target = $(e.target).parent();
-        let person_id = target.data('person_id');
-        let fname = target.data('fname');
-        let lname = target.data('lname');
+        let note_id = target.data('note_id');
+        let content = target.data('content');
 
         view.update_editor({
-            person_id: person_id,
-            fname: fname,
-            lname: lname,
+            note_id: note_id,
+            content: content,
         });
-
         view.set_button_state(view.EXISTING_NOTE);
     });
-
-    $('table').on('dblclick', 'tbody tr', e => {
-        let target = $(e.target);
-        let person_id = target.parent().attr('data-person_id');
-
-        window.location.href = `/people/${person_id}/notes`;
-
-    });
-
 }(ns.model, ns.view));
